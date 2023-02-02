d28f09222833e391f2763e93d860d3ee7d406321
Kelsey <rosella500@gmail.com>
2023-02-02 14:47:17 -0500

    Log when something's added to the alt cache

--- src/main/java/com/overseascasuals/recsbot/solver/Solver.java
+++ src/main/java/com/overseascasuals/recsbot/solver/Solver.java
@@ -553,6 +553,7 @@ public class Solver
 
         if(day+1>=Math.min(dayToSolve, 4)) //If we have the peaks for the day we're trying to solve
         {
+            LOG.info("Caching results for "+cacheKey);
             cachedAltRecs.put(cacheKey, recs);
         }
 
