41e8592b8ab9c8a6ad18c7b1ee7bbaccd4306c1e
Kelsey <rosella500@gmail.com>
2023-03-12 15:13:13 -0400

    Add transactional, I guess?

--- src/main/java/com/overseascasuals/recsbot/mysql/PopularityRepository.java
+++ src/main/java/com/overseascasuals/recsbot/mysql/PopularityRepository.java
@@ -1,7 +1,9 @@
 package com.overseascasuals.recsbot.mysql;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface PopularityRepository extends CrudRepository<Popularity, Integer> {
     Popularity findByWeek(int week);
 }
