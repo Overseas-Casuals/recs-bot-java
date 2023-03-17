97806694d9a54161cae484a0b811659e2265d38c
Kelsey <rosella500@gmail.com>
2023-03-17 05:20:18 -0400

    move sleep after stuff

--- src/main/java/com/overseascasuals/recsbot/BotConfiguration.java
+++ src/main/java/com/overseascasuals/recsbot/BotConfiguration.java
@@ -413,10 +413,11 @@ public class BotConfiguration implements CommandLineRunner
 
         LOG.info("Ran taskList {}", taskList);
 
-        Thread.sleep(5000); //Idk, just make sure things have a chance to finish running?
+
 
         //solver.getRestOfDayRecs(3, 20, 11, Item.GrowthFormula);
-        //solver.getRestOfDayRecs(5, 22, 11, null);
+        //solver.getRestOfDayRecs(3, 22, 11, null);
         //solver.getRecForSingleDay(5, 11, List.of(Item.GarnetRapier), true);
+        Thread.sleep(5000); //Idk, just make sure things have a chance to finish running?
     }
 }
